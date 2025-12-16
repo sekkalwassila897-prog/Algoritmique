c9bda9a0523a43f74604137f9f64f67be5b7dcfe
sekkalwassila897-prog <sekkalwassila897@gmail.com>
2025-12-16 17:05:02 +0100

    Un ajustement mineur dans TP1

--- TP1/app.py
+++ TP1/app.py
@@ -130,6 +130,9 @@ def home():
     </body>
     </html>
     """
+import os
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
