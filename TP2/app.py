33da970705960c856f8142a69ae811c6023cb9ee
sekkalwassila897-prog <sekkalwassila897@gmail.com>
2025-12-16 18:14:54 +0100

    modification dans tp2

--- TP2/app.py
+++ TP2/app.py
@@ -87,8 +87,7 @@ def home():
     </body>
     </html>
     """
-
+import os
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, threaded=True)
